7cbd04b007b72468dfc69129c219ccef2730cc67
Karim Gharbi <Mirak005>
2020-01-24 22:44:27 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,100 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Contact from "./components/contactCard";
+import { Button } from "@material-ui/core";
+import "./App.css";
 
 function App() {
+  // At first i tried to use each state in a const
+
+  // const [count , setCount] = useState(0)
+  // const [text , setText] = useState("Hey")
+  // const [users , setUsers] =useState([])
+  // const [user , setUser] = useState({
+  //   name:"",
+  //   email:""
+  //   })
+
+  //then i tried to complicate things and it works
+
+  const [state, setState] = useState({
+    count: 0,
+    text: "",
+    user: { name: "", email: "" },
+    users: []
+  });
+
+  const handelCount = () => setState({ ...state, count: state.count + 1 });
+  const handelReset = () => setState({ ...state, count: 0 });
+  const handelText = e => setState({ ...state, text: e.target.value });
+  const saveUser = (e, key) =>
+    setState({ ...state, user: { ...state.user, [key]: e.target.value } });
+  const handelAdd = () =>
+    setState({
+      ...state,
+      users: [...state.users, state.user],
+      user: { ...state.user, name: "", email: "" }
+    });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Hooks && @Material-ui</h1>
+      <div className="container">
+        <div className="click-count">
+          <p className="number-click"> You Clicked {state.count.toString().length<2 ? `0${state.count}`:state.count } Times</p>
+          <button className="click-me-btn" onClick={handelCount}>
+            Click me
+          </button>
+          <button
+            style={{ display: "block", margin: "0.5rem" }}
+            className="click-me-btn"
+            onClick={handelReset}
+          >
+            Reset Count
+          </button>
+        </div>
+        <div className="text-container">
+          <p className="text-change">{state.text ? state.text : "Change Me"}</p>
+          <label>Live Text Change </label>
+          <input
+            type="text"
+            maxLength="19"
+            value={state.text}
+            onChange={handelText}
+          />
+        </div>
+        <div className="users-container">
+          <h2>Add User with React hooks</h2>
+          <label>User Name</label>
+          <input
+            type="text"
+            name="name"
+            value={state.user.name}
+            onChange={event => saveUser(event, event.target.name)}
+          />
+          <label>Email</label>
+          <input
+            type="text"
+            name="email"
+            value={state.user.email}
+            onChange={e => saveUser(e, e.target.name)}
+          />
+
+          <Button
+            className="add-btn"
+            onClick={handelAdd}
+            variant="contained"
+            color="primary"
+          >
+            Add Contact
+          </Button>
+        </div>
+      </div>
+<div className="contact-list-container">
+
+      {state.users.map((user, index) => (
+        <Contact key={index} user={user} />
+      ))}
+</div>
     </div>
   );
 }
---
+++ src/components/contactCard.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import { red } from '@material-ui/core/colors';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+
+const useStyles = makeStyles(theme => ({
+  card: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+  avatar: {
+    backgroundColor: red[500],
+  },
+}));
+
+export default function RecipeReviewCard({user:{name , email}}) {
+  const classes = useStyles();
+  
+
+  return (
+    <Card className={classes.card}>
+      <CardHeader
+        avatar={
+          <Avatar aria-label="recipe" className={classes.avatar}>
+            {name[0].toUpperCase()}
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        // need props 
+        title={`@${name.split(" ")[0]}`}
+        subheader="September 14, 2016"
+      />
+     
+      <CardContent>
+      <Typography variant="body1"  component="p">
+{/* need props  */}
+          Name : {name.toUpperCase()}
+        </Typography>
+        <Typography variant="body2"  component="p">
+            {/* need props  */}
+
+          Email : {email}
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+        <IconButton aria-label="share">
+          <ShareIcon />
+        </IconButton>
+    
+      </CardActions>
+      
+    </Card>
+  );
+}
